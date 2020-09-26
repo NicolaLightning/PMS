f9ef8381675be642c9a1a356c58f46261e06cb55
xy <1055396761@qq.com>
2020-09-26 23:15:27 +0800

    WEB搭建

---
+++ Web/pms/src/views/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>登录页面</h1>
+    <el-input v-model="name" placeholder="请输入用户名" style="width: 300px"></el-input>
+    <el-input v-model="psw" placeholder="请输入密码" style="width: 300px" show-password></el-input>
+    <el-button type='primary'>默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  date () {
+    return {
+      name: '',
+      psw: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
